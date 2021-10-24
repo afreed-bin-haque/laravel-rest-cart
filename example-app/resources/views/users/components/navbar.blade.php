68b7b076ec2b9af855b4f2aee06ffc90c7275554
Afreed Bin Haque <cuatro.dev@gmail.com>
2021-10-25 01:34:03 +0600

    Dashboard Details

--- example-app/resources/views/users/components/navbar.blade.php
+++ example-app/resources/views/users/components/navbar.blade.php
@@ -5,18 +5,24 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-      <form method="POST" action="{{ route('logout') }}">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-color:#7FCB8D" type="button">
+          Hello {{ Auth::user()->name }}
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+            <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-
+                                <div class="d-grid gap-2">
                                 <a href="{{ route('logout') }}"
                                          onclick="event.preventDefault();
-                                                this.closest('form').submit();" class="btn btn-warning text-decoration-none" style="border-radius: 15px;">
-                                    Logout
+                                                this.closest('form').submit();" class="btn btn-outline-warning text-decoration-none">Log Out
                                 </a>
                             </form>
-                            </ul>
+            </li>
+          </ul>
+        </li>
     </div>
   </div>
 </nav>
--- example-app/resources/views/users/dashboard_view.blade.php
+++ example-app/resources/views/users/dashboard_view.blade.php
@@ -1,4 +1,22 @@
 @extends('users.dashboard_main')
 @section('dashboard_view')
-Hello <b style="color:#4361ee">{{ Auth::user()->name }}</b>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h4>Your Product Page</h4>
+    </div>
+    <div class="col">
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col">
+    <div class="card">
+  <div class="card-body">
+    This is some text within a card body.
+  </div>
+</div>
+    </div>
+  </div>
+</div>
 @endsection
